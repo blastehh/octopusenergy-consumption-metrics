89b0f829f5db673acd9e636776bbf618b953df94
Matthew Chan <me@blasteh.uk>
2022-12-09 19:10:16 +0000

    Frankenstein some code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Libraries
-const {InfluxDB, Point} = require('@influxdata/influxdb-client')
-const { toNanoDate } = require("influx")
+const process = require('process');
+const {
+    InfluxDB,
+    Point
+} = require('@influxdata/influxdb-client');
+const { toNanoDate } = require("influx");
+const { GraphQLClient, gql } = require('graphql-request');
 
 const axios = require('axios');
 const dotenv = require('dotenv');
-const sleep = require('./sleep')
-// Load dotenv
+const sleep = require('./sleep');
+
 dotenv.config();
 
 // Env Vars
@@ -22,128 +27,232 @@ const {
     INFLUXDB_ORG,
     INFLUXDB_BUCKET,
     LOOP_TIME,
-    PAGE_SIZE
-} = process.env
+    PAGE_SIZE,
+    OCTO_ACCOUNT_NUMBER
+} = process.env;
+
+const restRequest = url => {
+    url = `https://api.octopus.energy/v1${url}`;
+    return axios.get(url, {
+        auth: {
+            username: OCTO_API_KEY
+        }
+    });
+};
+
+const consumption = async ({ type, mp, sn, cost, writeApi }) => {
+    const url = `/${type}-meter-points/${mp}/meters/${sn}/consumption?page_size=${PAGE_SIZE}`;
+    const logmsg = `Polling ${type} consumption data from octopus API`;
+    console.log(logmsg);
+    const response = await restRequest(url)
+        .catch((e) => {
+            if (e.response) {
+                return Promise.reject(e.response.status);
+            } else if (response.status) {
+                return Promise.reject(response.status);
+            }
+            return Promise.reject(e);
+        });
+
+    for await (var point of response.data.results) {
+        const timestamp = toNanoDate(String(new Date(point.interval_end).valueOf()) + '000000');
+        await writeApi.writePoint(
+            new Point(type)
+                .floatField('consumption', Number(point.consumption))
+                .timestamp(timestamp));
+
+        if(cost) {
+            await writeApi.writePoint(
+                new Point(`${type}_cost`)
+                    .floatField('price', Number(point.consumption) * Number(OCTO_ELECTRIC_COST) / 100)
+                    .timestamp(timestamp));
+        };
+    }
+};
+
+const tariff = async (writeApi) => {
+    if(!OCTO_ACCOUNT_NUMBER) {
+        return Promise.resolve();
+    }
+
+    const endpoint = 'https://api.octopus.energy/v1/graphql/';
+    let client = new GraphQLClient(endpoint);
+    const token = await client.request(gql`
+      mutation obtainKrakenToken($key: String!) {
+        obtainKrakenToken(input: {APIKey: $key}) {
+          token
+        }
+      }
+    `, {
+        key: OCTO_API_KEY
+    }).then(response => response.obtainKrakenToken.token);
+
+    client = new GraphQLClient(endpoint, {
+        headers: {
+            Authorization: token
+        }
+    });
+    return client.request(gql`
+      query account($account: String!) {
+        account(accountNumber: $account) {
+          number
+          electricityAgreements {
+            tariff {
+              ... on StandardTariff {
+                displayName
+                fullName
+                productCode
+                tariffCode
+                standingCharge
+                unitRate
+              }
+            }
+          }
+          gasAgreements {
+            tariff {
+              displayName
+              fullName
+              productCode
+              tariffCode
+              standingCharge
+              unitRate
+          }
+        }
+      }
+    }`, {
+        account: OCTO_ACCOUNT_NUMBER
+    }).then(async ({ account }) => {
+        const timestamp = toNanoDate(String(new Date().valueOf()) + '000000');
+        await Promise.all(account.gasAgreements.map(async ({ tariff }) => {
+            await writeApi.writePoint(
+                new Point('gas_tariff')
+                    .timestamp(timestamp)
+                    .stringField('displayName', tariff.displayName)
+                    .stringField('fullName', tariff.fullName)
+                    .stringField('productCode', tariff.productCode)
+                    .stringField('tariffCode', tariff.tariffCode)
+                    .stringField('account', OCTO_ACCOUNT_NUMBER)
+                    .floatField('unitRate', Number(tariff.unitRate))
+                    .floatField('standingCharge', Number(tariff.standingCharge)));
+        }));
+        await Promise.all(account.electricityAgreements.map(async ({ tariff }) => {
+            await writeApi.writePoint(
+                new Point('electricity_tariff')
+                    .timestamp(timestamp)
+                    .stringField('displayName', tariff.displayName)
+                    .stringField('fullName', tariff.fullName)
+                    .stringField('productCode', tariff.productCode)
+                    .stringField('tariffCode', tariff.tariffCode)
+                    .stringField('account', OCTO_ACCOUNT_NUMBER)
+                    .floatField('unitRate', Number(tariff.unitRate))
+                    .floatField('standingCharge', Number(tariff.standingCharge)));
+        }));
+    });
+};
 
 const boot = async (callback) => {
-    console.log("Starting Octopus Energy Consumption Metrics Container")
-    console.log("Current Settings are:")
+    console.log("Starting Octopus Energy Consumption Metrics Container");
+    console.log("Current Settings are:");
     console.log(`
-        OCTO_API_KEY = ${OCTO_API_KEY}
+        OCTO_API_KEY = ${OCTO_API_KEY ? '*****' : undefined}
         OCTO_ELECTRIC_MPAN = ${OCTO_ELECTRIC_MPAN}
         OCTO_ELECTRIC_SN = ${OCTO_ELECTRIC_SN}
         OCTO_GAS_MPAN = ${OCTO_GAS_MPRN}
         OCTO_GAS_SN = ${OCTO_GAS_SN}
         INFLUXDB_URL = ${INFLUXDB_URL}
-        INFLUXDB_TOKEN = ${INFLUXDB_TOKEN}
+        INFLUXDB_TOKEN = ${INFLUXDB_TOKEN ? '*****' : undefined}
         INFLUXDB_ORG = ${INFLUXDB_ORG}
         INFLUXDB_BUCKET = ${INFLUXDB_BUCKET}
         LOOP_TIME = ${LOOP_TIME}
         OCTO_ELECTRIC_COST = ${OCTO_ELECTRIC_COST}
         OCTO_GAS_COST = ${OCTO_GAS_COST}
         PAGE_SIZE = ${PAGE_SIZE}
-    `)
+        OCTO_ACCOUNT_NUMBER = ${OCTO_ACCOUNT_NUMBER}
+    `);
 
 
     while (true){
         // Set up influx client
-        const client = new InfluxDB({url: INFLUXDB_URL, token: INFLUXDB_TOKEN})
-        const writeApi = client.getWriteApi(INFLUXDB_ORG, INFLUXDB_BUCKET)
-        writeApi.useDefaultTags({app: 'octopus-energy-consumption-metrics'})
-        console.log("Polling data from octopus API")
+        const client = new InfluxDB({
+            url: INFLUXDB_URL,
+            token: INFLUXDB_TOKEN
+        });
+        const writeApi = client.getWriteApi(INFLUXDB_ORG, INFLUXDB_BUCKET);
+        writeApi.useDefaultTags({
+            app: 'octopus-energy-consumption-metrics'
+        });
 
-        // Retrieve data from octopus API
-        let electricresponse = null;
-        let gasresponse = null;
-        try{
-            let options = {auth: {
-                username: OCTO_API_KEY
-            }}
-            if (OCTO_ELECTRIC_MPAN) {
-		    electricresponse = await axios.get(`https://api.octopus.energy/v1/electricity-meter-points/${OCTO_ELECTRIC_MPAN}/meters/${OCTO_ELECTRIC_SN}/consumption?page_size=${PAGE_SIZE}`, options)
-	    }
-            if (OCTO_GAS_MPRN) {
-		    gasresponse = await axios.get(`https://api.octopus.energy/v1/gas-meter-points/${OCTO_GAS_MPRN}/meters/${OCTO_GAS_SN}/consumption?page_size=${PAGE_SIZE}`, options)
-	    }
+        
+//        tariff(writeApi)
+//            .then(() => {
+//                console.log('Successfully polled tariff data');
+//            })
+//            .catch(e => {
+//                console.error(e);
+//                console.log('Error Polling tariff data');
+//            });
 
-            
-        } catch(e){
-            console.log("Error retrieving data from octopus API")
-            console.log(e)
-        }
+        
 
-        // Now we loop over every result given to us from the API and feed that into influxdb
-        if (electricresponse && electricresponse.status === 200) {
-            for await ( obj of electricresponse.data.results) {
-                // Here we take the end interval, and convert it into nanoseconds for influxdb as nodejs works with ms, not ns
-                const ts = new Date(obj.interval_end)
-                const nanoDate = toNanoDate(String(ts.valueOf()) + '000000')
-                
-                // work out the consumption and hard set the datapoint's timestamp to the interval_end value from the API
-                let electricpoint = new Point('electricity')
-                    .floatField('consumption', Number(obj.consumption))
-                    .timestamp(nanoDate)
-                
-		    
-                if (OCTO_ELECTRIC_COST) {  
-		    // Same again but for cost mathmatics
-		    let electriccost = Number(obj.consumption) * Number(OCTO_ELECTRIC_COST) / 100
+        await consumption({
+            writeApi,
+            type: 'electricity',
+            mp: OCTO_ELECTRIC_MPAN,
+            sn: OCTO_ELECTRIC_SN,
+            cost: OCTO_ELECTRIC_COST
+        })
+            .then(() => {
+                console.log('Successfully polled electricity consumption data');
+            })
+            .catch(e => {
+                console.error(e);
+                console.log('Error Polling electricity consumption data');
+            });
 
-                    let electriccostpoint = new Point('electricity_cost')
-                    .floatField('price', electriccost)
-                    .timestamp(nanoDate)
+        await consumption({
+            writeApi,
+            type: 'gas',
+            mp: OCTO_GAS_MPRN,
+            sn: OCTO_GAS_SN,
+            cost: OCTO_GAS_COST
+        })
+            .then(() => {
+                console.log('Successfully polled gas consumption data');
+            })
+            .catch(e => {
+                console.error(e);
+                console.log('Error Polling gas consumption data');
+            });
 
-                    writeApi.writePoint(electriccostpoint)
-                }
-
-                // and then write the points:
-                writeApi.writePoint(electricpoint)
-            }
-        }
-
-        if (gasresponse && gasresponse.status === 200) {
-            // Repeat the above but for gas
-            for await (obj of gasresponse.data.results) {
-                const ts = new Date(obj.interval_end)
-                const nanoDate = toNanoDate(String(ts.valueOf()) + '000000')
-    
-                let gaspoint = new Point('gas')
-                    .floatField('consumption', Number(obj.consumption))
-                    .timestamp(nanoDate)
-
-                if (OCTO_GAS_COST) {
-                    let gascost = Number(obj.consumption) * Number(OCTO_GAS_COST) / 100
-    
-                    let gascostpoint = new Point('gas_cost')
-                        .floatField('price', gascost)
-                        .timestamp(nanoDate)
-    
-                    writeApi.writePoint(gascostpoint)
-		}
-                writeApi.writePoint(gaspoint)
-    
-            }
-        }
+        console.log("Polling tariff data from octopus API");
+        await Promise.all([tariff(writeApi)])
+            .then( () => {
+                console.log('Successfully polled tariff data');
+            })
+            .catch((e) => {
+                console.error(e);
+                console.log('Error Polling tariff data');
+            });
 
         await writeApi
             .close()
             .then(() => {
-                console.log('Octopus API response submitted to InfluxDB successfully')
+                console.log('Octopus API response submitted to InfluxDB successfully');
             })
             .catch(e => {
-                console.error(e)
-                console.log('Error submitting data to InfluxDB')
-            })
-        
+                console.error(e);
+                console.log('Error submitting data to InfluxDB');
+            });
+
         // Now sleep for the loop time
-        console.log("Sleeping for: " + LOOP_TIME)
-         sleep(Number(LOOP_TIME))
+        console.log("Sleeping for: " + LOOP_TIME);
+        sleep(Number(LOOP_TIME));
     }
 }
 
 boot((error) => {
     if (error) {
-        console.error(error)
-        throw(error.message || error)
+        console.error(error);
+        throw(error.message || error);
     }
   });
